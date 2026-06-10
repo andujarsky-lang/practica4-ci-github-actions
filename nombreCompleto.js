c1c159fd10f76214caec3aed055dbf7ca3a6572d
Sky L. Andujar <andujarvictorinoskyluisahanie@gmail.com>
2026-06-10 18:01:14 -0400

    Agregar programa de nombre completo

---
+++ nombreCompleto.js
@@ -0,0 +1,6 @@
+let nombre = prompt("Ingrese su nombre:");
+let apellido = prompt("Ingrese su apellido:");
+
+let nombreCompleto = nombre + " " + apellido;
+
+alert("Tu nombre completo es: " + nombreCompleto);
